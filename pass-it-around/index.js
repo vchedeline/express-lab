27fe97ab158fa46cc46032dd2f109b9215ef9108
chedeline <vchedeline@gmail.com>
2022-04-02 21:06:33 -0400

    adds links for pass-it-around

--- pass-it-around/index.js
+++ pass-it-around/index.js
@@ -3,16 +3,36 @@ const app = express();
 const PORT = 3000;
 
 app.get("/", (req,res) => {
-  res.send(
+  res.send("No Bottles of Crémas On the Wall.");
+})
+
+app.get("/:num_of_bottles", (req,res) => {
+  if(parseInt(req.params.num_of_bottles) === 0) {
+    res.send(
+      `
+      0 Bottle of Crémas on the wall,
+      <html>
+        <body>
+          <a href='/'>add more Bottles of Crémas on the wall!</a>
+        </body>
+      </html>
+      `
+    );
+  }
+  else {
+    let takeOne = parseInt(req.params.num_of_bottles) - 1;
+    
+    res.send(
     `
-    99 Bottles of beer on the wall
+    ${req.params.num_of_bottles} Bottles of Crémas on the wall,
     <html>
       <body>
-        <a href='/98'>take one down, Pass it around</a>
+        <a href='/${takeOne}'>take one down, pass it around</a>
       </body>
     </html>
     `
-  );
+    );
+  }
 });
 
 app.listen(PORT, () => {
